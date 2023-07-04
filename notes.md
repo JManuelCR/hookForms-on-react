33d90bb6209bb5a8626c846cc848fa6edf3a8db4
Jose Manuel Cabrera Rojas <josemanuelcabrera2011@gmail.com>
2023-07-03 21:46:52 -0600

    clsx and forms

---
+++ notes.md
@@ -0,0 +1,19 @@
+# Librerias de react para manejo de formularios
+    Se necesita registrar el formulario
+    Concentrara la info del forulario en una variable
+
+    Se necesita usar una etiqueta form par apoder aprovechar el submit del formulario
+
+    Usar ala funsion handleSubmit dentro del submit par amanejar el submit
+
+    podemos obtener una lista de errores para tener un manejo de errores para podr desplegar que errores tenemos manejando
+
+## Librerias de componentes
+    Librerias de bootstrap
+    librerias de tailwind como daisy
+    importante separar lo quetenemos en etiquetado de lo que tenemos en estilos
+    no debemos casar el marcado con los estilos -> preguntar el final
+
+    librerias de classnames
+    classnames
+    clsx
--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { Koder } from "./types/common.types";
 import KoderCard from "./components/KoderCard";
+import clsx from "clsx";
 
 export default function App() {
   const [koders, setKoders] = useState<Koder[]>([]);
-  const [koderFirstName, setKoderFirstName] = useState("");
-  const [koderLastName, setKoderLastName] = useState("");
+  const { register, handleSubmit, reset, formState: { errors } } = useForm<Koder>()
 
-  function onAddItem() {
-    if (koderFirstName && koderLastName) {
-      const koder: Koder = {
-        firstName: koderFirstName,
-        lastName: koderLastName,
-      };
-      setKoders([koder, ...koders]);
-      setKoderFirstName("");
-      setKoderLastName("");
-    } else {
-      alert("Falta llenar un campo");
-    }
-  }
-
-  function onEnter(event: React.KeyboardEvent<HTMLInputElement>) {
-    if (event.key === "Enter") {
-      onAddItem();
-    }
+  function onSubmit(data: Koder) {
+    console.log("esta es la data", data);
+    setKoders([
+      { firstName: data.firstName, lastName: data.lastName },
+      ...koders
+    ]);
+    reset();
   }
 
   function onDelete(indexToDelete: number) {
@@ -34,39 +24,60 @@ export default function App() {
 
   function empty() {
     setKoders([]);
+    reset();
   }
+
+  const firstNameClassName = `bg-black border border-white/10 rounded w-full p-3 ${errors.firstName ? 'border-red-500' : "" }`
 
   return (
     <main className="bg-black min-h-screen flex items-center text-white p-10 flex-col gap-10">
       <h1 className="text-3xl">Koders</h1>
 
-      <section className="flex flex-row gap-2 w-full justify-center max-w-md">
+      <form  onSubmit={ handleSubmit(onSubmit)} className="flex flex-row gap-2 w-full justify-center max-w-md">
         <input
           id="text"
           type="text"
           placeholder="Nombre"
-          className="bg-black border border-white/10 rounded w-full p-3"
-          onChange={(event) => setKoderFirstName(event.target.value)}
-          onKeyUp={onEnter}
-          value={koderFirstName}
+          className={clsx(
+            "bg-black border border-white/10", 
+            "rounded w-full p-3",
+            { "border-red-500": errors.firstName}
+          )}
+          { ...register( 'firstName', { 
+            required: { value: true, message: 'El nombre es requerido'}, 
+            minLength: { value: 3, message: 'El nombre debe tener almenos 3 caracteres'} }
+            ) 
+          }
         />
         <input
           id="text"
           type="text"
           placeholder="Apellido"
-          className="bg-black border border-white/10 rounded w-full p-3"
-          onChange={(event) => setKoderLastName(event.target.value)}
-          onKeyUp={onEnter}
-          value={koderLastName}
+          className={clsx(
+            "bg-black border border-white/10", 
+            "rounded w-full p-3",
+            { "border-red-500": errors.lastName}
+          )}
+          { ...register( 'lastName', { 
+            required: { value: true, message: 'El apellido es requerido'}, 
+            minLength: { value: 3, message: 'El apellido debe tener almenos 3 caracteres'} }
+            ) 
+          }
         />
         <button
-          type="submit"
+          type="submit" // asegurarse que tiene el type submit
           className="bg-white text-black flex items-center justify-center p-2 rounded w-1/5"
-          onClick={onAddItem}
         >
           +
         </button>
-      </section>
+      </form>
+
+      {
+        errors.firstName && <p>{errors.firstName.message}</p>
+      }
+      {
+        errors.lastName && <p>{errors.lastName.message}</p>
+      }
 
       <section className="w-full flex flex-col gap-3">
         {koders.map((koder, index) => {
